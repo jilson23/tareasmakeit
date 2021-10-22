dce7151db83bcc7a6fe13250f8479773a42fc2e9
jilson jimenez <jilsonjimenez@gmail.com>
2021-10-21 19:49:04 -0500

    javascript modulo creacion

---
+++ javascript/loops.js
@@ -0,0 +1,41 @@
+// var i = 0;
+// while (i < 845) {
+//   console.log( i + " Hola mundo");
+//   i = i + 1;
+// }
+
+// for (var i = 0; i > -5; i = i-1) {
+//   console.log( i + " Hola mundo");
+// }
+
+// var i = 100;
+// while (i > 0) {
+//   console.log(i);
+//   i--;
+// }
+// var i = 0
+
+// while (i< 5) {
+//   console.log("Hola desde el cuerpo del ciclo");
+//   i++
+// }
+
+// var i = 0
+// while (i<2) {
+//   console.log("Hola desde el cuerpo del ciclo");
+//   i++
+// }
+
+// var i = 1
+// while (i<=50) {
+//   console.log(i+" Estoy aprendiendo");
+//   i++
+// }
+
+// for(var i = 1; i<=50; i++){
+//   console.log(i+" Estoy aprendiendo");
+// }
+
+for (var i = 0; i < 5; i ++) {
+  console.log("Hola mundo");
+}
---
+++ javascript/operador-ternario.js
@@ -0,0 +1,3 @@
+var num = 10;
+
+num >= 15 ? console.log('Es mayor o igual que 15') : console.log('Es menor que 15');
