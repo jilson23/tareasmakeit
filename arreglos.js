07e0dbe9fa66ea71c135a2e07246508bcb6698c4
jilson jimenez <jilsonjimenez@gmail.com>
2021-10-21 20:38:55 -0500

    arreglos

---
+++ arreglos.js
@@ -0,0 +1,9 @@
+// var array = ["Pedro"];
+// array.push("Germán"); // ["Pedro", "Germán"]
+// array.push("Diana");
+
+// var array = ["Pedro", "Germán", "Diana"];
+// array.splice(1, 0, "nik");
+
+// var array = ["Pedro", "Germán", "Diana"];
+// array.splice(1, 1); para eliminar un elemento del indice 1 en adelante
