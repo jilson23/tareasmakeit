625258e6092050bf357cd8df52bf9948f984edc6
jilson jimenez <jilsonjimenez@gmail.com>
2021-10-21 20:38:26 -0500

    resolver retos de arreglos y funciones

---
+++ javascript/bmi.js
@@ -0,0 +1,29 @@
+// function bmi(weight, height) {
+//     return weight / height ** 2
+//   }
+  
+//   console.log("Tu IMC es: " + bmi(80, 1.8));
+
+// function greetings(nombre, apellido) {
+// return `Mi nombre es ${nombre} y mi apellido es ${apellido}`
+// };
+// console.log(greetings("Nayib", "Abdalá"));
+// console.log(greetings("Germán", "Escobar"));
+// console.log(greetings("Simón", "Hoyos"));
+
+// function sum(val1,val2) { 
+//     return val1 + val2;
+// }
+// console.log(sum(10, 20));  // 30
+// console.log(sum(10, 10));  // 20
+// console.log(sum(40, 60));  // 100
+// console.log(sum(50, 200));  // 250
+
+
+// function sayMyName(name) {
+//     return ("Tu nombre es " + name);
+//   }
+  
+//   var name = "Nayib";
+//   console.log(sayMyName(name));
+
